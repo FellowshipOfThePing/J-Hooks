df69a5949b5a51e9e00e247bdd8d79f57440e775
Jeremy <jeremysbinder@gmail.com>
2021-05-26 20:41:15 -0700

    Added tests for useWindowSize.

---
+++ examples/useWindowSize.example.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { useWindowSize } from "../src";
+
+const UseWindowSizeExample = () => {
+  const [width, height] = useWindowSize();
+
+  return (
+    <div>
+      <div>Width: {width}</div>
+      <div>Height: {height}</div>
+    </div>
+  );
+};
+
+export { UseWindowSizeExample };
